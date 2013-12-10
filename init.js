049fa5f8ba3e431c6b633f5b00acbbcd9ba13998
jonrandoem <contact@jonrandoem.com>
2013-12-10 15:21:13 +0100

    Prevents multiple display of the same QR if button is pressed multiple times

--- init.js
+++ init.js
@@ -1,23 +1,21 @@
 function generateQrCode(id) {
 	try {
 		var query = "?op=pluginhandler&plugin=qrcodegen&method=getQr&id=" + param_escape(id);
-
-		console.log(query);
-
 		new Ajax.Request("backend.php", {
 			parameters: query,
 			onComplete: function(transport) {
 				var reply = transport.responseText;
-
 				var elem = $("CWRAP-" + id);
-
 				if (elem) {
-					elem.innerHTML = elem.innerHTML + '<br><br>' + reply;
+					var el = $("QR-" + id);
+					if ( el ) {
+						el.innerHTML = '<br><br>' + reply;
+					} else {
+						elem.innerHTML = elem.innerHTML + '<div id="QR-' + id + '"><br><br>' + reply + '</div>';
+					}
 				}
 			}
 		});
-
-
 	} catch (e) {
 		exception_error("generateQrCode", e);
 	}
